a305b4ab06e0873f89303fd9a0760a8d7e305942
kyoyoung <keumky2@gmail.com>
2022-09-23 20:31:23 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./src/theme.config.js",
+  unstable_staticImage: true,
+  unstable_flexsearch: {
+    codeblock: false,
+  },
+});
 
-module.exports = nextConfig
+module.exports = withNextra({
+  reactStrictMode: true,
+});
